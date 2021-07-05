a3fcb76411e1b1e9899ae2c9f80ff7009904e5fa
Sunil Kumar Yadav <52737895+SunilKuYadav@users.noreply.github.com>
2021-07-05 18:54:25 +0530

    Delete age.js

--- age.js
+++
@@ -1,6 +0,0 @@
-let dobData = "2031-01-06";
-let dob = new Date(dobData);
-var today = new Date();
-console.log(today);
-var age = Math.floor((today-dob) / (365.25 * 24 * 60 * 60 * 1000));
-console.log(age)
